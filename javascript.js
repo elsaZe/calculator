f92cfd13ac3afa2d5d707bfaad5d343bdc7943ff
Elsa Zeigler <elsa.head@gmail.com>
2022-06-06 21:02:03 -0400

    Add the four operations

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+console.log(add(3,4))
+console.log(subtract(3,4))
+console.log(multiply(3,4))
+console.log(divide(3,4))
