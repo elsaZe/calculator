bf15c4a02afaa793821ee99c6914d582d4fc2355
Elsa Zeigler <elsa.head@gmail.com>
2022-06-10 07:09:38 -0400

    reset after using equals button

--- javascript.js
+++ javascript.js
@@ -4,111 +4,187 @@ let operation = ''
 const display = document.getElementById('display');
 
 function updateDisplay(num) {
-    if (currentNumber.length<11) {
+    let length = num.toString().length
+    if (length>10) {
+        display.textContent = "ERR 2 LRG"
+    } else {
         display.textContent = num
     }
 }
 
 const zero = document.getElementById('zero')
 zero.addEventListener('click', () => {
-    currentNumber = currentNumber+'0';
+    currentNumber = currentNumber+'0'
     updateDisplay(currentNumber)})
 
 const one = document.getElementById('one')
 one.addEventListener('click', () => {
-    currentNumber = currentNumber+'1';
-    updateDisplay(currentNumber)})
+    if (temp != undefined && currentNumber == '' && operation == '') {
+        temp = undefined
+        currentNumber = "1";
+        updateDisplay(currentNumber)
+    } else {
+        currentNumber = currentNumber+'1'
+        updateDisplay(currentNumber)
+    }
+})
 
 const two = document.getElementById('two')
 two.addEventListener('click', () => {
-    currentNumber = currentNumber+'2';
-    updateDisplay(currentNumber)})
+    if (temp != undefined && currentNumber == '' && operation == ''){
+        temp = undefined
+        currentNumber = '2'
+        updateDisplay(currentNumber)
+    } else {
+        currentNumber = currentNumber+'2'
+        updateDisplay(currentNumber)}
+})
 
 const three = document.getElementById('three')
 three.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == '' && operation == ''){
+        temp = undefined;
+        currentNumber = '3';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'3';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
 
 const four = document.getElementById('four')
 four.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == '' && operation == ''){
+        temp = undefined;
+        currentNumber = '4';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'4';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
 
 const five = document.getElementById('five')
 five.addEventListener('click', () => {
-    currentNumber = currentNumber+'5';
-    updateDisplay(currentNumber)})
+    if (temp != undefined && currentNumber == ''&& operation == ''){
+        temp = undefined;
+        currentNumber = '5';
+        updateDisplay(currentNumber)
+    } else {
+        currentNumber = currentNumber+'5';
+         updateDisplay(currentNumber)}
+})
 
 const six = document.getElementById('six')
 six.addEventListener('click', () => {
-    currentNumber = currentNumber+'6';
-    updateDisplay(currentNumber)})
+    if (temp != undefined && currentNumber == '' && operation == ''){
+        temp = undefined;
+        currentNumber = '6';
+        updateDisplay(currentNumber)
+    } else {
+        currentNumber = currentNumber+'6';
+        updateDisplay(currentNumber)}
+})
 
 const seven = document.getElementById('seven')
 seven.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == ''&& operation == ''){
+        temp = undefined;
+        currentNumber = '7';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'7';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
 
 const eight = document.getElementById('eight')
 eight.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == ''&& operation == ''){
+        temp = undefined;
+        currentNumber = '8';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'8';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
         
 const nine = document.getElementById('nine')
 nine.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == ''&& operation == ''){
+        temp = undefined;
+        currentNumber = '3';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'9';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
 
 const decimal = document.getElementById('decimal')
 decimal.addEventListener('click', () => {
+    if (temp != undefined && currentNumber == ''&& operation == ''){
+        temp = undefined;
+        currentNumber = '.';
+        updateDisplay(currentNumber)
+    } else {
     currentNumber = currentNumber+'.';
-    updateDisplay(currentNumber)})
+    updateDisplay(currentNumber)}
+})
 
 const addition = document.getElementById('add')
 addition.addEventListener('click', () => {
-        if(temp == undefined) {
+        console.log("temp :"+ temp+ "CN: " + currentNumber)
+        if(temp == undefined && currentNumber != '') {
             temp = currentNumber;
             currentNumber = ''
-            operation = 'add'}
-        else {
             operation = 'add'
-            operate()
+        } else if (temp !== undefined && currentNumber == ''){
+            operation = 'add'
+        } else if (temp != undefined && currentNumber != '') {
+            operate();
+            operation = 'add'
         }
+        return temp;
  })
 
 const subtraction = document.getElementById('subtract')
 subtraction.addEventListener('click', () => {
-    if(temp == undefined){
+    if(temp == undefined && currentNumber != ''){
         temp = currentNumber;
         currentNumber = '';
         operation = 'subtract';
-    } else {
-        operation ='subract';
+    } else if (temp !== undefined && currentNumber == '') {
+        operation = 'subtract'
+    } else if (temp != undefined && currentNumber != ''){
         operate();
+        operation ='subtract';
     }
+    console.log("end temp " + temp)
+    console.log("end CN " + currentNumber)
 })
 
 const multiplication = document.getElementById('multiply')
 multiplication.addEventListener('click', () => {
-    if(temp == undefined) {
+    if(temp == undefined && currentNumber != ''){
         temp = currentNumber;
         currentNumber = '';
         operation = 'multiply';
-    } else {
+    } else if (temp !== undefined && currentNumber == '') {
         operation = 'multiply'
+    } else if (temp != undefined && currentNumber != ''){
         operate();
+        operation ='multiply';
     }
 })
 
 const division = document.getElementById('divide')
 division.addEventListener('click', () => {
-    if(temp == undefined){
+    if(temp == undefined && currentNumber != ''){
         temp = currentNumber;
-        currentNumber = ''
+        currentNumber = '';
+        operation = 'divide';
+    } else if (temp !== undefined && currentNumber == '') {
         operation = 'divide'
-    } else {
-        operation = 'divide'
+    } else if (temp != undefined && currentNumber != ''){
         operate();
+        operation = 'divide'
     }
 })
 
@@ -118,32 +194,41 @@ percentage.addEventListener('click', () => {
         operate();
     })
 
-const changeSign = document.getElementById('posOrNeg')
-changeSign.addEventListener('click', () => {
-        if (temp == undefined) {
-            temp = currentNumber
-            operation = 'changeSign'
+const squareRoot = document.getElementById('sqRt')
+squareRoot.addEventListener('click', () => {
+        if (temp == undefined && currentNumber != ''){
+            temp = currentNumber;
+            currentNumber = '';
+            operation = 'sqRt';
             operate();
-        } else {
-            operation = 'changeSign'
+        } else if (temp != undefined && currentNumber == '') {
+            operation = 'sqRt';
+            operate();
+        } else if (temp != undefined && currentNumber != ''){
+            operate();
+            operation = 'sqRt';
             operate();
         }
     })
 
 const exponentiation = document.getElementById('exponent')
 exponentiation.addEventListener('click', () => {
-    if(temp == undefined){
+    if(temp == undefined && currentNumber != ''){
         temp = currentNumber;
-        currentNumber = ''
+        currentNumber = '';
+        operation = 'exponent';
+    } else if (temp !== undefined && currentNumber == '') {
         operation = 'exponent'
-    } else {
-        operation = 'exponent'
+    } else if (temp != undefined && currentNumber != ''){
         operate();
+        operation = 'exponent'
     }
 })
 
 const equals = document.getElementById('equals');
-equals.addEventListener('click', operate)
+equals.addEventListener('click', () => {
+    operate()
+    operation = ''})
 
 const clear = document.getElementById('clear');
 clear.addEventListener('click', () => {
@@ -153,50 +238,49 @@ clear.addEventListener('click', () => {
 
 
 function operate() {
-    console.log("start CN " +currentNumber)
-    console.log("start temp: "+temp)
+    console.log("temp :"+ temp+ "CN: " + currentNumber)
     currentNumber = Number(currentNumber);
     temp = Number(temp);
     if(operation == 'add') {
         sum = add(currentNumber, temp)
-        display.textContent = sum;
+        updateDisplay(sum);
         temp = sum;
         currentNumber = ''
     } else if (operation == 'subtract'){
         difference = subtract(currentNumber, temp)
-        display.textContent = difference;
+        console.log("subtract")
+        updateDisplay(difference);
         temp = difference;
         currentNumber = ''
     } else if (operation == 'multiply'){
         product = multiply(currentNumber, temp)
-        display.textContent = product;
+        updateDisplay(product);
         temp = product;
         currentNumber = ''
     } else if (operation == 'divide'){
         quotient = divide(currentNumber, temp);
-        display.textContent = quotient;
+        updateDisplay(quotient);
         temp = quotient;
         currentNumber = ''
     } else if (operation == 'percent') {
         numToPercent = percent(currentNumber)
-        display.textContent = numToPercent+'%'
+        updateDisplay(numToPercent+'%')
         temp = numToPercent;
         currentNumber = ''
     } else if (operation == 'exponent'){
         power = exponent(temp, currentNumber)
-        display.textContent = power;
+        updateDisplay(power);
         temp = power;
         currentNumber = ''
-    } else if (operation == 'changeSign'){
-        opposite = sign(temp);
-        display.textContent = opposite;
-        temp = '';
-        currentNumber = opposite;
+    } else if (operation == 'sqRt'){
+            sqRt = root(temp);
+            updateDisplay(sqRt);
+            temp = sqRt;
+       
+        }
+        return temp;
     }
-    console.log('temp: ' + temp);
-    console.log('currentNumber: ' + currentNumber)
-    return temp;
-}
+    
 
 function add(num1, num2) {
     return num1 + num2
@@ -219,10 +303,10 @@ function percent(num){
 }
 
 function exponent(num1, num2) {
-    return num1**num2;
+    return Math.round(num1**num2*10000)/10000;
 }
 
-function sign(num) {
-    return -num;
+function root(num) {
+    return Math.round(Math.sqrt(num)*10000)/10000;
 }
 
